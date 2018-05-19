e8767167f68e8cc40e69f6ced0803f675559b1b1
DzirtDrow <34365803+DzirtDrow@users.noreply.github.com>
2018-05-19 14:29:19 +0300

    ddl and init_sql changed

--- internetstore-database/src/main/resources/init_data.sql
+++ internetstore-database/src/main/resources/init_data.sql
@@ -1,3 +1,12 @@
-INSERT INTO internet_store_db.`order`(user_id, order_date, sum) VALUES (2,DATE('2018-5-11'),100);
+INSERT INTO `internet_store_db`.`user` ( name, lastname, email, password, birthdate, role) VALUES
+  ("admin", "admin", "admin@admin.ru", "$2a$10$23pqbny5gsxIIae1aSi.1uzyDJViFnlpr.zYtBp8fRwdqHZFBc0Du", "1990-12-12","admin");
 
-INSERT INTO internet_store_db.cart (goods_id, order_id, count) VALUES (26, 2, 2);
+
+INSERT INTO `internet_store_db`.`category` (name) VALUES ("SSD");
+
+INSERT INTO `internet_store_db`.`goods` (name, price, description, left_count, category_id)
+    VALUES ("500 Gb SSD", 150, "500 Gb SSD", 15, (SELECT id FROM category WHERE category.name = "SSD"));
+
+
+INSERT INTO `internet_store_db`.`goods` (name, price, description, left_count, category_id)
+VALUES ("1000 Gb SSD", 350, "1000 Gb SSD", 15, (SELECT id FROM category WHERE category.name = "SSD"));
