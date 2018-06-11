8a5c8aa4a257867a2b944bdd756c2fb616feef32
DzirtDrow <34365803+DzirtDrow@users.noreply.github.com>
2018-06-11 18:12:57 +0300

    REST Webservices Testing, RabbitMQ testing1

--- internetstore-web/src/main/java/com/tsystems/javaschool/brajnikov/internetstore/configuration/RabbitConfig.java
+++ internetstore-web/src/main/java/com/tsystems/javaschool/brajnikov/internetstore/configuration/RabbitConfig.java
@@ -4,11 +4,14 @@ import com.tsystems.javaschool.brajnikov.internetstore.webservices.TestMQControl
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,7 +20,6 @@ public class RabbitConfig{
 
     static final Logger logger = LoggerFactory.getLogger(TestMQController.class);
 
-    //настраиваем соединение с RabbitMQ
     @Bean
     public ConnectionFactory connectionFactory() {
         CachingConnectionFactory connectionFactory =
@@ -35,24 +37,23 @@ public class RabbitConfig{
         return new RabbitTemplate(connectionFactory());
     }
 
-    //объявляем очередь с именем queue1
     @Bean
     public Queue myQueue1() {
         return new Queue("queue1");
     }
 
     //объявляем контейнер, который будет содержать листенер для сообщений
-//    @Bean
-//    public SimpleMessageListenerContainer messageListenerContainer1() {
-//        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-//        container.setConnectionFactory(connectionFactory());
-//        container.setQueueNames("queue1");
-//        container.setMessageListener(new MessageListener() {
-//            //тут ловим сообщения из queue1
-//            public void onMessage(Message message) {
-//                logger.info("received from queue1 : " + new String(message.getBody()));
-//            }
-//        });
-//        return container;
-//    }
+    @Bean
+    public SimpleMessageListenerContainer messageListenerContainer1() {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory());
+        container.setQueueNames("queue1");
+        container.setMessageListener(new MessageListener() {
+            //тут ловим сообщения из queue1
+            public void onMessage(Message message) {
+                logger.info("received from queue1 : " + new String(message.getBody()));
+            }
+        });
+        return container;
+    }
 }
